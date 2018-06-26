a9f7de47492e573b48de90002110622782541ed9
D. Gormash <dlgormash@gmail.com>
2018-06-26 15:53:50 +0500

    init

--- EDCreator/Pages/Login.xaml.cs
+++ EDCreator/Pages/Login.xaml.cs
@@ -12,28 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using EDCreator.Pages;
 
-namespace EDCreator
+namespace EDCreator.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Page
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Main.Content = new Login();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Main.Content = new WorkshopArea();
         }
     }
 }
--- EDCreator/Pages/WorkshopArea.xaml.cs
+++ EDCreator/Pages/WorkshopArea.xaml.cs
@@ -12,28 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using EDCreator.Pages;
 
-namespace EDCreator
+namespace EDCreator.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WorkshopArea.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WorkshopArea : Page
     {
-        public MainWindow()
+        public WorkshopArea()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Main.Content = new Login();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Main.Content = new WorkshopArea();
         }
     }
 }
