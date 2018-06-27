d6c8a49cd26061bbfc4f6e0f60460968f64dce64
D. Gormash <dlgormash@gmail.com>
2018-06-27 15:37:50 +0500

    Добавил появление диалога открытия файлов при нажатии на кнопку [...]. OpenFileDialog _opener - это приватное поле, которое я в дальнейшем буду использовать в процедуре обработки файлов

--- EDCreator/Pages/WorkshopArea.xaml.cs
+++ EDCreator/Pages/WorkshopArea.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Microsoft.Win32;
 
 namespace EDCreator.Pages
 {
@@ -23,6 +28,28 @@ namespace EDCreator.Pages
         public WorkshopArea()
         {
             InitializeComponent();
+        }
+
+        private void Choose_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDlg = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "PDF files (*.pdf)|*.pdf",
+                InitialDirectory = $"{System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\in"
+            };
+
+            if (openFileDlg.ShowDialog() == true)
+            {
+                var pdfReader = new PdfReader(openFileDlg.FileName);
+                var sb = new StringBuilder();
+                for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                {
+                    sb.Append(PdfTextExtractor.GetTextFromPage(pdfReader, i));
+                }
+                FileList.Text = sb.ToString();
+                pdfReader.Close();
+            }
         }
     }
 }
