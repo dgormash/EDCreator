b418e77b198f989178d629e51e740186c15bf301
D. Gormash <dlgormash@gmail.com>
2018-06-28 23:27:48 +0500

    Чуть поправил положение, теперь данные считаются правильно, но не все

--- EDCreator/Logic/AbstractPdfParser.cs
+++ EDCreator/Logic/AbstractPdfParser.cs
@@ -46,36 +46,43 @@ namespace EDCreator.Logic
             var rect = new iTextSharp.text.Rectangle(296, 722, 323, 728);
             renderFilter[0] = new RegionTextRenderFilter(rect);
             transferingData.Name = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
-
+            
             //SerialNumber
             rect = new iTextSharp.text.Rectangle(437, 722, 460, 728);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             transferingData.SerialNumber = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
 
             //Length shoulder to shoulder
             rect = new iTextSharp.text.Rectangle(148, 640, 165, 647);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             transferingData.Length = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
 
             //Connection 1 OD
             rect = new iTextSharp.text.Rectangle(109, 564, 130, 570);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             var connection = new Connection();
             connection.Od = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
 
             //Connection 1 BOX Tread Size
             rect = new iTextSharp.text.Rectangle(103, 600, 118, 606);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             var sb = new StringBuilder();
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(119, 600, 126, 606);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(103, 592, 105, 598);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(105, 592, 125, 598);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
 
             connection.Box = sb.ToString();
@@ -84,27 +91,33 @@ namespace EDCreator.Logic
             //Connection 2 OD
             rect = new iTextSharp.text.Rectangle(347, 564, 368, 570);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             connection = new Connection();
             connection.Od = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
 
             //Connection 2 ID
             rect = new iTextSharp.text.Rectangle(437, 722, 460, 728);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             connection.Id = PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy);
 
             //Connection 2 BOX Tread Size
             rect = new iTextSharp.text.Rectangle(339, 600, 355, 606);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb = new StringBuilder();
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(355, 600, 362, 606);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(339, 592, 341, 598);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
             rect = new iTextSharp.text.Rectangle(342, 592, 362, 598);
             renderFilter[0] = new RegionTextRenderFilter(rect);
+            textExtractionStrategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), renderFilter);
             sb.Append(PdfTextExtractor.GetTextFromPage(reader, 1, textExtractionStrategy));
 
             connection.Box = sb.ToString();
