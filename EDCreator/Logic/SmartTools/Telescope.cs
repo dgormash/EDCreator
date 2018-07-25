f8da4c968c9d2e37a564a09d76310bebc428c1f5
D. Gormash <dlgormash@gmail.com>
2018-07-25 11:00:59 +0500

    Добавил класс Telescope, представляющий изделие Телескоп из трёх частей

---
+++ EDCreator/Logic/SmartTools/Telescope.cs
@@ -0,0 +1,11 @@
+﻿using FDCreator.Misc;
+
+namespace FDCreator.Logic.SmartTools
+{
+    public class Telescope:ISmartTool, ITopPart, IMiddlePart, IBottomPart
+    {
+        public IParsedData Top { get; set; }
+        public IParsedData Middle { get; set; }
+        public IParsedData Bottom { get; set; }
+    }
+}
