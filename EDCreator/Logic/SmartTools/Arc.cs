099d76de12541717e151bb8f00418f9c81fb6633
D. Gormash <dlgormash@gmail.com>
2018-07-25 11:04:46 +0500

    Добавил класс Arc, представляющий изделие arc, состоящее из двух частей

---
+++ EDCreator/Logic/SmartTools/Arc.cs
@@ -0,0 +1,10 @@
+﻿using FDCreator.Misc;
+
+namespace FDCreator.Logic.SmartTools
+{
+    public class Arc : ISmartTool, ITopPart,  IBottomPart
+    {
+        public IParsedData Top { get; set; }
+        public IParsedData Bottom { get; set; }
+    }
+}
