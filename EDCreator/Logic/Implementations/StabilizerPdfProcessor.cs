3a3086bc69150963096188f97b66e7596b88da58
D. Gormash <dlgormash@gmail.com>
2018-08-02 17:11:32 +0500

    Добавил новую реализацию интерфейса IPdfProcessor под шаблон Stabilizer

---
+++ EDCreator/Logic/Implementations/StabilizerPdfProcessor.cs
@@ -0,0 +1,14 @@
+﻿using FDCreator.Logic.Interfaces;
+using FDCreator.Misc;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class StabilizerPdfProcessor:IPdfProcessor
+    {
+        private IConnectionInfoAssembler _connetionInfoAssembler = new ConnectionInfoAssembler();
+        public IParsedData GetParsedDataFromPdf(string file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
