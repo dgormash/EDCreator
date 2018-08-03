0001f231e3f34e6dde9b55ffd34249ad8b602071
D. Gormash <dlgormash@gmail.com>
2018-08-03 15:21:00 +0500

    Добавил классы CrossoverExcelProcessorNpoiVersionCreator и NmdcExcelProcessorNpoiVersionCreator для создания соответствующих версий процессоров

---
+++ EDCreator/Logic/Implementations/CrossoverExcelProcessorNpoiVersionCreator.cs
@@ -0,0 +1,12 @@
+﻿using FDCreator.Logic.Interfaces;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class CrossoverExcelProcessorNpoiVersionCreator:IExcelProcessorCreator
+    {
+        public IExcelProcessor GetProcessor()
+        {
+            return new CrossoverExcelProcessorNpoiVersion();
+        }
+    }
+}
