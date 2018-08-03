21ec6e02443c86eefc7acc4e8b29584e274e8606
D. Gormash <dlgormash@gmail.com>
2018-08-03 11:38:35 +0500

    Добавил интерфейс ICellValueWriter для записи данных в ячейки excel при использовании NPOI

---
+++ EDCreator/Logic/Interfaces/ICellValueWriter.cs
@@ -0,0 +1,10 @@
+﻿using NPOI.SS.UserModel;
+
+namespace FDCreator.Logic.Interfaces
+{
+    public interface ICellValueWriter
+    {
+        ISheet Sheet { get; set; }
+        void SetCellValue(int rowNum, int cellNum, string value);
+    }
+}
