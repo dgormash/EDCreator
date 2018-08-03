6efabb486475ce283919c1ff81c5a95f0a5172a5
D. Gormash <dlgormash@gmail.com>
2018-08-03 11:42:48 +0500

    Добавил класс CellValueWriter, реализующий интерфейс ICellValueWriter для NPOI

---
+++ EDCreator/Logic/Implementations/CellValueWriter.cs
@@ -0,0 +1,18 @@
+﻿using FDCreator.Logic.Interfaces;
+using NPOI.SS.UserModel;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class CellValueWriter:ICellValueWriter
+    {
+        public ISheet Sheet { get; set; }
+        private IRow _row;
+        private ICell _cell;
+        public void SetCellValue(int rowNum, int cellNum, string value)
+        {
+            _row = Sheet.GetRow(rowNum);
+            _cell = _row.GetCell(cellNum);
+            _cell.SetCellValue(value);
+        }
+    }
+}
