20daa3db18c6246464994af7342ad531c48facd0
D. Gormash <dlgormash@gmail.com>
2018-08-03 14:40:44 +0500

    Добавил интерфейс IExcelProcessorCreator и его реализацию, возвращающую StandartExcelProcessorNpoiVersion

---
+++ EDCreator/Logic/Implementations/StandartExcelProcessorNpoiVersionCreator.cs
@@ -0,0 +1,12 @@
+﻿using FDCreator.Logic.Interfaces;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class StandartExcelProcessorNpoiVersionCreator:IExcelProcessorCreator
+    {
+        public IExcelProcessor GetProcessor()
+        {
+            return new StandartExcelProcessorNpoiVersion();
+        }
+    }
+}
