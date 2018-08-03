219b4ec4dd11331083682bfd2e9f7fda209de826
D. Gormash <dlgormash@gmail.com>
2018-08-03 16:11:24 +0500

    Добавил классы DumbIronClient и PdfPreparser. Первый будет содержать логику вызова создания соответствующих процессоров, вызов их и передачу им данных, второй извлекает имя (тулкод) из файла инспекции

---
+++ EDCreator/Logic/DumbIronClient.cs
@@ -0,0 +1,89 @@
+﻿using System.Windows;
+using FDCreator.Logic.Implementations;
+using FDCreator.Logic.Interfaces;
+using FDCreator.Misc;
+
+namespace FDCreator.Logic
+{
+    public class DumbIronClient
+    {
+        private HeaderData _header;
+        private string _toolCode;
+        private IPdfProcessor _pdfProcessor;
+        private IExcelProcessor _excelProcessor;
+        private IPdfProcessorCreator _pdfProcessorCreator;
+        private IExcelProcessorCreator _excelProcessorCreator;
+
+        public HeaderData Header
+        {
+            set { _header = value; }
+        }
+
+        public void Run(string file)
+        {
+            //1. Определяем тулкод изделия
+            var preparser = new PdfPreparser();
+            _toolCode = preparser.GetParsedDataFromPdf(file);
+
+            var firstLetters = GetFirstLettersOfToolCode(_toolCode);
+
+            switch (firstLetters) 
+            {                       
+                case "MFS":
+                case "SFS":
+                case "NMPC":
+                    _pdfProcessorCreator = new StandartPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new StandartExcelProcessorNpoiVersionCreator();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                case "SZS":
+                    _pdfProcessorCreator = new StabilizerPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new StablizerExcelProcessorNpoiVersion();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                case "SBS":
+                    _pdfProcessorCreator = new StandartPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new NearBitSubExcelProcessorNpoiVersionCreator();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                case "SZB":
+                    _pdfProcessorCreator = new StabilizerPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new NearBitSubStablizerExcelProcessorNpoiVersion();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                case "NMDC":
+                    _pdfProcessorCreator = new StandartPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new NmdcExcelProcessorNpoiVersionCreator();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                case "SXO":
+                    _pdfProcessorCreator = new StandartPdfProcessorCreator();
+                    _pdfProcessor = _pdfProcessorCreator.GetProcessor();
+                    _excelProcessorCreator = new CrossoverExcelProcessorNpoiVersionCreator();
+                    _excelProcessor = _excelProcessorCreator.GetProcessor();
+                    break;
+                default:
+                    MessageBox.Show(
+                        "A nonstandart name was received while reading the file. Perhaps there is no handler for the file, or the file is not an inspection file",
+                        "Warining", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    return;
+            }
+        }
+
+        private static string GetFirstLettersOfToolCode(string toolCode)
+        {
+            var substringableValue = toolCode.ToUpper();
+            if (substringableValue.StartsWith("NMPC") || substringableValue.StartsWith("NMDC"))
+            {
+                return substringableValue.Substring(0, 4);
+            }
+
+            return substringableValue.Substring(0, 3);
+        }
+    }
+}
---
+++ EDCreator/Logic/Implementations/PdfPreparser.cs
@@ -0,0 +1,15 @@
+﻿using iTextSharp.text;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class PdfPreparser
+    {
+        private readonly IPdfParser _parser = new PdfParser();
+        
+        public string GetParsedDataFromPdf(string file)
+        {
+            var rect = new Rectangle(279, 727, 309, 732);
+            return _parser.GetStringValueFromRegion(file, rect);
+        }
+    }
+}
