49833b526de2671622133f0e6e61cff66f2e349f
D. Gormash <dlgormash@gmail.com>
2018-08-04 20:07:51 +0500

    Добавил класс GdisTelescopeExcelProcessorNpoiVersionCreator, реализующий интерфейс ISmartToolExcelProcessorCreator

---
+++ EDCreator/Logic/Implementations/GdisTelescopeExcelProcessorNpoiVersionCreator.cs
@@ -0,0 +1,12 @@
+﻿using FDCreator.Logic.Interfaces;
+
+namespace FDCreator.Logic.Implementations
+{
+    public class GdisTelescopeExcelProcessorNpoiVersionCreator:ISmartToolExcelProcessorCreator
+    {
+        public ISmartToolExcelProcessor GetProcessor()
+        {
+            return new GdisTelescopeExcelProcessorNpoiVersion();
+        }
+    }
+}
